38e57284d1dfbcd4e0b1c67be9fbe5f2ad2e9331
Kelly <88280337+NguKel19@users.noreply.github.com>
2026-04-18 21:04:06 +0200

    Implement TextEncoder and TextDecoder polyfills
    Add polyfills for TextEncoder and TextDecoder if not defined

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,23 @@
-import { TextEncoder, TextDecoder } from 'util';
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = class TextEncoder {
+    constructor() {}
+    encode(str) {
+      return new Uint8Array(Array.from(str).map(c => c.charCodeAt(0)));
+    }
+  };
+}
 
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = class TextDecoder {
+    constructor() {}
+    decode(arr) {
+      return String.fromCharCode.apply(null, arr);
+    }
+  };
+}
 
-Object.defineProperty(global, 'TextEncoder', { value: TextEncoder });
-Object.defineProperty(global, 'TextDecoder', { value: TextDecoder });
+globalThis.TextEncoder = global.TextEncoder;
+globalThis.TextDecoder = global.TextDecoder;
 
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
