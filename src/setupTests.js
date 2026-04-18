7ed061adfeee83e0788159c940341199e7e7f38e
Kelly <88280337+NguKel19@users.noreply.github.com>
2026-04-18 21:12:57 +0200

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,25 +1,5 @@
-if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = class TextEncoder {
-    constructor() {}
-    encode(str) {
-      return new Uint8Array(Array.from(str).map(c => c.charCodeAt(0)));
-    }
-  };
-}
-
-if (typeof global.TextDecoder === 'undefined') {
-  global.TextDecoder = class TextDecoder {
-    constructor() {}
-    decode(arr) {
-      return String.fromCharCode.apply(null, arr);
-    }
-  };
-}
-
-globalThis.TextEncoder = global.TextEncoder;
-globalThis.TextDecoder = global.TextDecoder;
-
 import '@testing-library/jest-dom';
 import { vi } from 'vitest';
 
+// Allows us to use 'vi' globally for mocking if needed
 global.vi = vi;
