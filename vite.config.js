7c50843464fc2bef3bbe6757ff353b976cfd13a4
Kelly <88280337+NguKel19@users.noreply.github.com>
2026-04-18 22:41:35 +0200

    Update test setupFiles in vite.config.js
    Removed 'vitest-setup.js' from setupFiles in test configuration.

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js', './vitest-setup.js'],
+    setupFiles: ['./src/setupTests.js'],
     css: true,
     
     alias: {
